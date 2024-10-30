a39c1be1f4632e9033d65501b32ba4f2248e191a
Broken_Head <young990420@gmail.com>
2024-10-31 00:07:24 +0900

    Homework REMIND And Redesigned
    숙제 재설계

--- Assets/01_Scripts/Grid.cs
+++ Assets/01_Scripts/Grid.cs
@@ -12,16 +12,25 @@ public class Grid : MonoBehaviour
         CreateGrid();
     }
 
-    public void CreateGrid()
+    public void CreateGrid() //그리드 생성
     {
-        Grids = new Node[GridSize.x, GridSize.y];
+        Grids = new Node[GridSize.x, GridSize.y]; //그리드 크기 정의
 
         for(int x = 0; x < GridSize.x; x++)
         {
             for(int y = 0; y < GridSize.y; y++)
             {
                 Vector2Int gridPos = new Vector2Int(x, y);
-                bool isWalkable = true;
+                int isObstruction = Random.Range(0, 10);
+                bool isWalkable;
+                if (isObstruction <= 0)
+                {
+                    isWalkable = false;
+                }
+                else
+                {
+                    isWalkable = true;
+                }
                 Grids[x, y] = new Node(gridPos, isWalkable);
             }
         }
@@ -30,6 +39,18 @@ public class Grid : MonoBehaviour
     public List<Node> NearbyNodes(Node node)
     {
         List<Node> nearNodes = new List<Node>();
+        for (int x = -1; x <= 1; x++)
+        {
+            for(int y = -1; y <= 1; y++)
+            {
+                if(x == 0 && y == 0)
+                {
+                    continue;
+                }
+
+            }
+        }
+
 
         return nearNodes;
     }
